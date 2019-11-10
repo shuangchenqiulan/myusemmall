93fdf25ee5c00391f739ed869ad7fc8b16eb5adb
zhangfan <zhangfan@m.scnu.edu.cn>
2019-11-10 11:58:46 +0800

    第一次提交

--- springdata/src/main/java/com/imooc/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.imooc.repository;
-
-import com.imooc.domain.Employee;
-import org.springframework.data.repository.Repository;
-
-public interface EmployeeRepository extends Repository<Employee,Integer> {
-    Employee findByName(String name);
-}
